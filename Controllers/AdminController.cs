e3f89215872c5ef39382b60b88d220897e5d5bfc
ana004 <paulaana08002019@gmail.com>
2022-09-06 15:22:01 -0300

    Criada parte da avaliacao

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcRazorViews.ViewModels;
+namespace MvcRazorViews.Controllers;
+
+public class AdminController : Controller 
+{
+  // private static List<ComercioViewModel> comercios = new List<ComercioViewModel>();
+    private static List<SiteViewModel> comercios = new List<SiteViewModel>
+    {
+        new SiteViewModel(1, "Piso 1", "Cerâmica", "Aqui vende cerâmica", "Loja", "cer@gmial.com"),
+        new SiteViewModel(2, "Piso 2", "Cerâmica2", "Aqui vende cerâmica2", "Loja2", "cer2@gmial.com"),
+        new SiteViewModel(3, "Piso 3", "Cerâmica3", "Aqui vende cerâmica3", "Loja3", "cer4@gmial.com"),
+        new SiteViewModel(4, "Piso 2", "Cerâmica2", "Aqui vende cerâmica2", "Loja2", "cer2@gmial.com")
+    };
+
+    public IActionResult Index()
+    {
+        return View(comercios);
+    }
+    
+     public IActionResult Show(int id)
+    {
+        return View(comercios[id-1]);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        comercios.RemoveAt(id-1);
+
+        return Index();
+    }
+
+    public IActionResult Create([FromForm] int id, [FromForm] string piso, [FromForm] string nome, [FromForm] string descricao, [FromForm] string tipo, [FromForm] string email)
+    {
+        return View(new SiteViewModel(id, piso, nome, descricao, tipo, email));
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LG3Avaliacao_Ana.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
