d7839d645261c2150a08fd995f3e8d218bb40078
ana004 <paulaana08002019@gmail.com>
2022-09-19 22:13:09 -0300

    Alteradas as funcionalidades de deletar e mostrar, e realizadas outras alteracoes

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,7 +4,7 @@ namespace MvcRazorViews.Controllers;
 
 public class AdminController : Controller 
 {
-  // private static List<ComercioViewModel> comercios = new List<ComercioViewModel>();
+//   private static List<SiteViewModel> comercios = new List<SiteViewModel>();
     private static List<SiteViewModel> comercios = new List<SiteViewModel>
     {
         new SiteViewModel(1, "Piso 1", "Cerâmica", "Aqui vende cerâmica", "Loja", "cer@gmial.com"),
@@ -17,22 +17,62 @@ public class AdminController : Controller
     {
         return View(comercios);
     }
+
+    public IActionResult SiteIndex()
+    {
+        return View(comercios);
+    } 
     
      public IActionResult Show(int id)
     {
-        return View(comercios[id-1]);
+        int i = 0;
+        foreach(var comercio in comercios)
+        {
+            if(comercio.Id == id)
+                {
+                    return View(comercios[i]);
+                }
+            ++i;
+        }
+         return View(comercios[i]);
     }
 
-    public void Delete(int id)
+    public IActionResult Delete(int id)
     {
-        comercios.RemoveAt(id-1);
+        int i = 0;
+        foreach(var comercio in comercios)
+        {
+            if(comercio.Id == id)
+                {
+                    comercios.RemoveAt(i);
+                    return RedirectToAction(nameof(Index));
+                }
+            ++i;
+        }
+        
+        return RedirectToAction(nameof(Index));
     }
 
-    public IActionResult Create([FromForm] int id, [FromForm] string nome, [FromForm] string piso, [FromForm] string descricao, [FromForm] string tipo, [FromForm] string email)
+    public IActionResult Create()
     {
-        SiteViewModel novoComercio = new SiteViewModel(id, piso, nome, descricao, tipo, email);
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create([FromForm] string nome, [FromForm] string piso, [FromForm] string descricao, [FromForm] string tipo, [FromForm] string email)
+    {
+        comercioExistsByName(nome);
+        int id = 4;
+        id++;
+        SiteViewModel novoComercio = new SiteViewModel(id, nome, piso, descricao, tipo, email);
         comercios.Add(novoComercio);
         return View();
+    }
+
+    private void comercioExistsByName(string name) 
+    {    
+        if(comercios.Any(e => e.Nome.Equals(name)))
+            throw new Exception($"O nome {name} já existe");   
     }
 
 }
--- ViewModels/SiteViewModel.cs
+++ ViewModels/SiteViewModel.cs
@@ -4,18 +4,19 @@ public class SiteViewModel
 {
     public int Id { get; set; }
     public string Piso { get; set; }
-    public string Nome { get; set; }
+    public string Nome {  get; set; }
     public string Descricao { get; set; }
     public string Tipo { get; set; }
     public string Email { get; set; }
 
-    public SiteViewModel(int id, string piso, string nome, string descricao, string tipo, string email)
+    public SiteViewModel(int id, string nome, string piso, string descricao, string tipo, string email)
     {
         Id = id;
-        Piso = piso;
         Nome = nome;
+        Piso = piso;
         Descricao = descricao;
         Tipo = tipo;
         Email = email;
     }
+    
 }
